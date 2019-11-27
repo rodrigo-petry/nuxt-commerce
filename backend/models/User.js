0516891e00ee82a6277b33324312db78f1d921aa
Rodrigo Petry <rodrigo.petry18@gmail.com>
2019-11-26 22:15:24 -0300

    Separando Backend do Frontend em Repositórios Diferentes

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  lastname: String,
-  email: String
-})
-
-module.exports = mongoose.model('User', UserSchema)
